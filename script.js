efa470b0f84e170bf202f9f6ae8786243cf39f97
chelsyehannum <chelsye.hannum@gmail.com>
2018-07-30 23:21:42 -0400

    added comments

--- script.js
+++ script.js
@@ -3,22 +3,23 @@
 $(document).ready(() => {
     let table;
 
+    // reservation form pops up when table is clicked on
     $(".available").on("click", (e) => {
         table = $(e.target)
         $("form").show();
 
     });
-
+    // reservation form "hides" when sent and table now reads "reserved"
     $("button").click(function() {
         $("form").hide();
         table.removeClass("available").addClass("reserved")
 
     });
-
+    //X button on form
     $(".material-icons").click(function() {
         $("form").hide();
     });
-
+    //available table changes color when hovered over
     $("body").on("mouseenter mouseleave", ".container .available", (e) => {
         $(e.target).toggleClass("hover");
     });
